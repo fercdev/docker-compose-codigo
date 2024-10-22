6e42758ad52fe5f6fb17d34341e2dfcc898f4f45
Luis Fernando <lcruzf@intercorp.com.pe>
2024-10-21 21:10:21 -0500

    ejemplo actualizado mysql nuevo servicio

--- frontend/angular-frontend/src/app/app.component.ts
+++ frontend/angular-frontend/src/app/app.component.ts
@@ -10,13 +10,34 @@ import { DataService } from './data.service';
 export class AppComponent implements OnInit{
   data: any[] = [];
   title = 'angular-frontend';
+  users : any[] = [];
+
+  firstName: string = '';
+  lastName: string = '';
+  age: number = 0;
 
   constructor(private dataService: DataService){}
 
   ngOnInit(): void {
-    
-    this.dataService.obtenerData().subscribe(response=>{
-      this.data = response;
+    this.loadUsers();
+  }
+
+  addUser() {
+    const newUser = {
+      firstName: this.firstName,
+      lastName: this.lastName,
+      age: this.age,
+    };
+
+    this.dataService.addUser(newUser).subscribe(() => {
+      console.log('Usuario creado');
+      this.loadUsers();
+    });
+  }
+
+  loadUsers() {
+    this.dataService.getUsers().subscribe((data: any) => {
+      this.users = data;
     })
   }
 }
--- frontend/angular-frontend/src/app/data.service.ts
+++ frontend/angular-frontend/src/app/data.service.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  private apiUrl = 'http://localhost:3000/data'
+  private apiUrl = 'http://localhost:3000/api/users'
 
   constructor(private http: HttpClient) { }
 
-  obtenerData(): Observable<any> {
-    return this.http.get<any>(this.apiUrl);
+  getUsers() {
+    return this.http.get(this.apiUrl);
+  }
+
+  addUser(user: {firstName: string, lastName:string, age: number}) {
+    return this.http.post(this.apiUrl, user);
   }
 }
